cd62027493d318a41371cb62df5508b6567576b3
Chang-Yu-Chang <ray57512@gmail.com>
2022-06-15 11:56:17 -0400

    lab network and renv

--- scripts/lab_network.R
+++ scripts/lab_network.R
@@ -5,43 +5,27 @@ library(rvest)
 
 
 pub_xpath <- '//*[@id="block-yui_3_17_2_1_1567798471690_21270"]/div/p'
+
+journals <- c("PLoS Biology", "PNAS", "Cell Systems", "eLife", "ISME", 
+              "Frontiers", "Annual", "Natur", "Current", "Science", "Scientific",
+              "Evolution", "PL", "Lecture", "Bio", "mS", "Comm", "Journal")
+
 coauthor_list <- read_html("http://www.sanchezlaboratory.com/pubs") %>%
     # Select the line of publication list
     html_elements("section") %>%
-    # Separate individual publicaitons
+    # Separate individual publications
     html_elements("p") %>%
     html_text() %>%
     # Separate titles and authors
-    strsplit(split = "\\(\\d{4}\\)") %>% sapply(function(x) x[2]) %>%
-    # Clean up shits
-    str_replace("^\\.\\s+", "") %>%
-    str_replace("\\#|\\*", "") %>%
-    str_replace("^\\s+", "") %>%
+    str_split("\\(\\d{4}\\)") %>% sapply(function(x) x[2]) %>% 
     # Separate authors and journals
-    strsplit(split = "\\. Cell Systems") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. eLife") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. ISME") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. PNAS") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Frontiers") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\.\\s+PL") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\s+PL") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Annual") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Natur") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Current") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Science") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Evolution") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Bio") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Lecture") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. Journal") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\. mS") %>% sapply(function(x) x[1]) %>%
-    strsplit(split = "\\w Communi") %>% sapply(function(x) x[1]) %>%
+    str_split(paste0("(", paste(journals, collapse = ")|("), ")")) %>% sapply(function(x) x[1]) %>%
     `[`(!is.na(.)) %>%
     # Separate names
-    strsplit(split = "\\s*,\\s*") %>%
-    sapply(function(x) strsplit(x, split = " \\& ")) %>%
-    lapply(unlist) %>%
-    sapply(function(x) strsplit(x, split = " and ")) %>%
-    lapply(unlist)
+    str_split(",|( and )|(,and )|&")  %>%
+    # Make a tibble
+    lapply(function(x) tibble(Author = x)) %>%
+    bind_rows(.id = "Paper") 
 
 sanchez_lab <- c(
     "Chang-Yu Chang",
@@ -64,34 +48,34 @@ sanchez_lab <- c(
 sanchez_lab <- rev(sanchez_lab)
 
 
-sanchez_lab_g1 <- c(    "Chang-Yu Chang",
-                        "Jean Vila",
-                        "Maria Rebolleda-Gomez",
-                        "Nanxi Lu",
-                        "Sylvie Estrela",
-                        "Alicia Sanchez-Gorostiaga",
-                        "Djordje Bajic",
-                        "Alvaro Sanchez",
-                        "Joshua Goldford",
-                        "Madeline Bender")
+sanchez_lab_g1 <- c("Chang-Yu Chang",
+                    "Jean Vila",
+                    "Maria Rebolleda-Gomez",
+                    "Nanxi Lu",
+                    "Sylvie Estrela",
+                    "Alicia Sanchez-Gorostiaga",
+                    "Djordje Bajic",
+                    "Alvaro Sanchez",
+                    "Joshua Goldford",
+                    "Madeline Bender")
 
-
-# Clean up space in the beginning or the end
-paper_author_list <-  coauthor_list %>%
-    lapply(function(x) tibble(Author = x)) %>%
-    bind_rows(.id = "Paper") %>%
+# Clean up author names
+paper_author_list <- coauthor_list %>%
     mutate(Author = str_replace(Author, "^\\s", "")) %>%
     mutate(Author = str_replace(Author, "\\s$", "")) %>%
-    mutate(Author = str_replace(Author, "#$", "")) %>%
-    mutate(Author = str_replace(Author, "\\*$", "")) %>%
-    # Manually add in Cell system paper
-    bind_rows(tibble(Paper = rep("1", 2), Author = c("Joshua Goldford", "Alicia Sanchez-Gorostiaga")))
+    mutate(Author = str_replace(Author, "^\\.", "")) %>%
+    mutate(Author = str_replace(Author, "\\.$", "")) %>%
+    mutate(Author = str_replace(Author, "\\#$", "")) %>%
+    mutate(Author = str_replace(Author, "\\*$", "")) %>% 
+    mutate(Author = str_replace(Author, "^\\s", "")) %>%
+    mutate(Author = str_replace(Author, "\\s$", ""))
 
 # Clean up the name ambiguity
 paper_author_list_sanchez <- paper_author_list %>%
     mutate(Author = ifelse(Author %in% c("Jean CC Vila", "Jean C.C. Vila", "Jean C. C. Vila"), "Jean Vila", Author)) %>%
     mutate(Author = ifelse(Author %in% c("María Rebolleda-Gomez"), "Maria Rebolleda-Gomez", Author)) %>%
     mutate(Author = ifelse(Author %in% c("Djordje Bajić", "Djordje Bajic"), "Djordje Bajic", Author)) %>%
+    mutate(Author = ifelse(Author %in% c("Sanchez A"), "Alvaro Sanchez", Author)) %>%
     filter(Author %in% sanchez_lab)
 
 # Nodes
@@ -129,8 +113,8 @@ node_size = 3
 p1 <- graph %>%
     ggraph(layout = "linear") +
     geom_edge_arc(aes(edge_width = `Number of coauthored papers`), color = "grey20", alpha = 0.5,
-        start_cap = circle(node_size/2+1, "mm"),
-        end_cap = circle(node_size/2+1, "mm")) +
+                  start_cap = circle(node_size/2+1, "mm"),
+                  end_cap = circle(node_size/2+1, "mm")) +
     geom_node_text(aes(label = name, color = Gen1), size = 3, hjust = 1) +
     coord_flip() +
     scale_edge_width(breaks = c(1,4,9)) +
@@ -148,8 +132,8 @@ p2 <- graph %>%
     activate(nodes) %>% filter(name %in% sanchez_lab_g1) %>%
     ggraph(layout = "circle") +
     geom_edge_link(aes(edge_width = `Number of coauthored papers`), color = "grey20", alpha = 0.5,
-        start_cap = circle(node_size/2+1, "mm"),
-        end_cap = circle(node_size/2+1, "mm")) +
+                   start_cap = circle(node_size/2+1, "mm"),
+                   end_cap = circle(node_size/2+1, "mm")) +
     geom_node_text(aes(label = name), size = 3, color = "black") +
     theme_classic() +
     scale_edge_width(breaks = c(1,4,9)) +
